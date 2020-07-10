9d193a93cac604ffb7b836cec4565055e5a257fa
DJ Viking <anders.swedishviking@gmail.com>
2020-07-10 01:34:42 -0400

    beginning the prepending process of the city name with the text field

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -10,18 +10,41 @@
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
 
-
+// have the forecast unhide after selecting the city and updating to the document
 
 //document variables
 //  form input textarea element
 //  
+//select textarea element
 const formInputTextArea = document.querySelector("#city-name");
 //  button element
-const buttonEl = document.querySelector("#submit-button");
-
-
+const buttonEl = document.querySelector("#search-button");
+//select the city list element we are prepending the city name to
+const citylistEl = document.querySelector("#city-list");
 
 //submit button function
-//  submit button div creation
+function displaySearchedCity(){
+    event.preventDefault();
+    //check button was clicked
+    console.log("search button was clicked")
+    //remove the hide class before we append
+    citylistEl.classList.remove("hide-before-append");
+    //get the value of the text field to place into the city span element
+    let cityName = document.querySelector("#city-name").value;
+    cityName.value = "";
+    //create element containing city name
+    let cityEl = document.createElement("span");
+    //set class list for city name container
+    cityEl.classList = "slight-margin-allaround width-100 border-bottom-user"
+    //check we are getting it in the console
+    console.log("here is the city name")
+    console.log(cityName);
+}
 
 //submit button event listener
+buttonEl.addEventListener("click", displaySearchedCity);
+
+
+
+
+
