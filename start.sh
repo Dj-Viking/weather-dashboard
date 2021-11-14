411727b21299b47c46c7f53aeaa6ae1f9361a144
dj-viking <anders.swedishviking@gmail.com>
2021-11-13 22:36:34 -0500

    correcting echo, heroku is ephemeral and read only, if i need to update the code locally and then push it to heroku again

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 cd src;
 if [ -d "dist" ]; then
-  echo "found dist directory, deleting and recompiling, starting server..."
+  echo "found dist directory, starting server..."
   npm run start
 elif ! [ -d "dist" ]; then
   echo "no dist folder detected, compiling typescript, and then starting server"
