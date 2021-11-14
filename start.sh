6bdeedc2bf22bd003b3133bc26ef868ada886f82
dj-viking <anders.swedishviking@gmail.com>
2021-11-13 22:17:43 -0500

    if i change server code, i will just remotely delete the dist and then recompile

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 cd src;
 if [ -d "dist" ]; then
   echo "found dist directory, deleting and recompiling, starting server..."
-  rm -rf dist;
-  npm run tsc;
   npm run start
 elif ! [ -d "dist" ]; then
   echo "no dist folder detected, compiling typescript, and then starting server"
